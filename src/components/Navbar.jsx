98c0f005b0a6f4bed78848a16138a12fd06dbbca
ahmetercankaplan <ahmetercankaplan6@gmail.com>
2025-12-03 01:50:29 +0100

    Update API URL to Render backend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -114,7 +114,7 @@ const Navbar = () => {
             )}
           </div>
 
-          <div className="flex items-center space-x-3">
+          <div className="hidden md:flex items-center space-x-3">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="sm" data-testid="language-selector">
